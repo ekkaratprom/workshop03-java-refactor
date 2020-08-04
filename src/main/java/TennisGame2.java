b74db25985e938b4d8220450050c9e6ddcdd7018
Ekkarat Promrit <ekkaratpro@mathregit.group>
2020-08-04 13:16:18 +0700

    :hammer: : update README.md refactor-analysis v.3

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,6 +1,6 @@
 public class TennisGame2 {
-    public int player1Point = 0;
-    public int player2Point = 0;
+    public int player1Point;
+    public int player2Point;
 
 
     private String player1Name;
@@ -15,7 +15,7 @@ public class TennisGame2 {
         String player1Result = "";
         String player2Result = "";
         String score = "";
-        if (player1Point == player2Point && player1Point < 4) {
+        if (player1Point == player2Point && player1Point < 4) { //equalScore
             if (player1Point == 0)
                 score = "Love";
             if (player1Point == 1)
@@ -24,10 +24,10 @@ public class TennisGame2 {
                 score = "Thirty";
             score += "-All";
         }
-        if (player1Point == player2Point && player1Point >= 3)
+        if (player1Point == player2Point && player1Point >= 3) //equalScore && point >= 3
             score = "Deuce";
 
-        if (player1Point > 0 && player2Point == 0) {
+        if (player1Point > 0 && player2Point == 0) { //onlyPlayer1HavePointScore
             if (player1Point == 1)
                 player1Result = "Fifteen";
             if (player1Point == 2)
@@ -35,10 +35,10 @@ public class TennisGame2 {
             if (player1Point == 3)
                 player1Result = "Forty";
 
-            player2Result = "Love";
+            player2Result = "Love"; // resultFormat Duplicate
             score = player1Result + "-" + player2Result;
         }
-        if (player2Point > 0 && player1Point == 0) {
+        if (player2Point > 0 && player1Point == 0) { //onlyPlayer2HavePointScore
             if (player2Point == 1)
                 player2Result = "Fifteen";
             if (player2Point == 2)
@@ -46,11 +46,11 @@ public class TennisGame2 {
             if (player2Point == 3)
                 player2Result = "Forty";
 
-            player1Result = "Love";
+            player1Result = "Love"; //  resultFormat Duplicate
             score = player1Result + "-" + player2Result;
         }
 
-        if (player1Point > player2Point && player1Point < 4) {
+        if (player1Point > player2Point && player1Point < 4) { // player1Score
             if (player1Point == 2)
                 player1Result = "Thirty";
             if (player1Point == 3)
@@ -59,9 +59,9 @@ public class TennisGame2 {
                 player2Result = "Fifteen";
             if (player2Point == 2)
                 player2Result = "Thirty";
-            score = player1Result + "-" + player2Result;
+            score = player1Result + "-" + player2Result; //  resultFormat Duplicate
         }
-        if (player2Point > player1Point && player2Point < 4) {
+        if (player2Point > player1Point && player2Point < 4) { // player2Score
             if (player2Point == 2)
                 player2Result = "Thirty";
             if (player2Point == 3)
@@ -70,31 +70,31 @@ public class TennisGame2 {
                 player1Result = "Fifteen";
             if (player1Point == 2)
                 player1Result = "Thirty";
-            score = player1Result + "-" + player2Result;
+            score = player1Result + "-" + player2Result; // return resultFormat Duplicate
         }
 
-        if (player1Point > player2Point && player2Point >= 3) {
+        if (player1Point > player2Point && player2Point >= 3) { //hard code --> advantagePlayer()
             score = "Advantage player1";
         }
 
-        if (player2Point > player1Point && player1Point >= 3) {
+        if (player2Point > player1Point && player1Point >= 3) { //hard code --> advantagePlayer()
             score = "Advantage player2";
         }
 
-        if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2) {
+        if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2) { //hard code --> endGame()
             score = "Win for player1";
         }
-        if (player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2) {
+        if (player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2) { //hard code --> endGame()
             score = "Win for player2";
         }
         return score;
     }
 
-    public void player1Score() {
+    public void player1Score() { // Duplicate method --> receivePoint()
         player1Point++;
     }
 
-    public void player2Score() {
+    public void player2Score() { // Duplicate method --> receivePoint()
         player2Point++;
     }
 
