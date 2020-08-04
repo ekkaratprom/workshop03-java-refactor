033dba2e8c52e2805026e39e37efcb551982b74b
Ekkarat Promrit <ekkaratpro@mathregit.group>
2020-08-04 22:32:11 +0700

    :hammer: : combine equalScore() and normalScore() to setScore()

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -16,13 +16,14 @@ public class TennisGame2 {
 
         // equal score not deuce
         if (player1Point == player2Point && player1Point < 3) { //equalScore
-            return equalScore(player1Point);
+            return setScore(player1Point) + "-All";
         }
 
         // Deuce
-        if (player1Point == player2Point ) //equalScore && point >= 3
-            return  "Deuce";
+        if (player1Point == player2Point) //equalScore && point >= 3
+            return "Deuce";
 
+        // Win
         if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2) {
             return "Win for " + player1Name;
         }
@@ -31,8 +32,9 @@ public class TennisGame2 {
             return "Win for " + player2Name;
         }
 
+        // Advantage
         if (player1Point > player2Point && player2Point >= 3) { //hard code
-            return  "Advantage " + player1Name;
+            return "Advantage " + player1Name;
         }
 
         if (player2Point > player1Point && player1Point >= 3) { //hard code
@@ -40,7 +42,29 @@ public class TennisGame2 {
         }
 
         //normal case
-        return formatResult(normalScore(player1Point),normalScore(player2Point));
+        return formatResult(setScore(player1Point), setScore(player2Point));
+    }
+
+    public String setScore(int playerPoint) {
+        String score = "";
+        if (playerPoint == 0) {
+            score = "Love";
+        }
+        if (playerPoint == 1) {
+            score = "Fifteen";
+        }
+        if (playerPoint == 2) {
+            score = "Thirty";
+        }
+        if (playerPoint == 3) {
+            score = "Forty";
+        }
+        return score;
+
+    }
+
+    public String formatResult(String s1, String s2) {
+        return s1 + "-" + s2;
     }
 
     public void player1Score() {
@@ -49,35 +73,6 @@ public class TennisGame2 {
 
     public void player2Score() {
         player2Point++;
-    }
-
-    public String equalScore(int playerPoint){
-        String score = "";
-        if (playerPoint == 0)
-            score = "Love";
-        if (playerPoint == 1)
-            score = "Fifteen";
-        if (playerPoint == 2)
-            score = "Thirty";
-        return score + "-All";
-    }
-
-    public String normalScore(int playerPoint){
-        String score = "";
-        if (playerPoint == 0)
-            score = "Love";
-        if (playerPoint == 1){
-            score =  "Fifteen";}
-        if (playerPoint == 2){
-            score = "Thirty";}
-        if (playerPoint == 3){
-            score = "Forty";}
-        return score;
-
-    }
-
-    public String formatResult(String s1,String s2){
-        return s1 + "-" + s2;
     }
 
 
